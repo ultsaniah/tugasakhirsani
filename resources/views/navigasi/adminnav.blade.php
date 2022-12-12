8757987e746eb04738329f771a98b21c424b19f7
ultsaniah <ultsaniah11@gmail.com>
2022-12-12 13:27:19 +0700

    asd

--- resources/views/navigasi/adminnav.blade.php
+++ resources/views/navigasi/adminnav.blade.php
@@ -2,7 +2,7 @@
 <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
     <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
         <h1>
-            <a href="{{ route('beranda') }}" style="text-decoration: none; color: black;">
+            <a style="text-decoration: none; color: black;">
                 Himari Craft
             </a>
             {{-- Himari --}}
